d324765055273998e979da50b5ddc637449f0ce5
Olga Ryzhova <olya_saleeva@mail.ru>
2024-02-27 22:57:23 +0400

    Вторая версия шестой домашней работы

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -11,27 +11,45 @@ form.addEventListener('submit', (event) => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = "json";
   
-
-    xhr.addEventListener('load', () => {
-        if (xhr.readyState === xhr.DONE) {
-            let user =  xhr.response;
-            localStorage.setItem('user_id', user.user_id);
-            
-            console.log(user); 
-            if (user.success === false) {
-                alert('Неверный логин/пароль');
-            } else if (user.success === true) {
-                signin.classList.remove('signin_active');
-                welcome.classList.add('welcome_active');
-                welcome.textContent += user.user_id;    
-            }
-        }         
+    xhr.addEventListener('load', () => {   
+        let user =  xhr.response;
+        localStorage.setItem('user_id', user.user_id);
+        
+        console.log(user); 
+        if (user.success === false) {
+            alert('Неверный логин/пароль');
+        } else if (user.success === true) {
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+            welcome.textContent += user.user_id;    
+        }           
     })
-
+   
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     const formData = new FormData(form);
 	xhr.send(formData);
+
+    const forms = event.currentTarget;
+    forms.reset();
 });
+
+window.addEventListener('load', () => {
+    const user = localStorage.getItem('user_id');
+    if (user === 'undefined') {
+        welcome.classList.remove('welcome_active');
+    } else if (user) {
+        signin.classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        welcome.textContent += user;    
+    } else {
+        return;
+    }
+});
+
+
+ 
+
+
 
 
 
